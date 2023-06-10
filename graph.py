edd162b470ad0419006d7c4eb47749022cfb0efc
seo0o519 <seoyoung344@naver.com>
2023-06-10 17:24:26 +0100

    issue #1 add mpl.use('Agg')

--- graph.py
+++ graph.py
@@ -1,8 +1,8 @@
 from cgi import parse_qs
-from template import html
-import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.use('Agg')
+from template import html
+import matplotlib.pyplot as plt
 def application(environ, start_response):
     if environ['PATH_INFO'] == '/img/graph.png':
         try:
